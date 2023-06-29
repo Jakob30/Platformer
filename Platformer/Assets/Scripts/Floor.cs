5065d1aaf9030c61ef9df371f06e8f2a135fd635
Jakob Renneisen <jakob.renneisen@web.de>
2023-06-29 18:45:11 +0200

    3_commit

---
+++ Platformer/Assets/Scripts/Floor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    public GameObject tm_coll;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Platformer/Assets/Scripts/Player.cs
+++ Platformer/Assets/Scripts/Player.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
 
 public class Player: MonoBehaviour
 {
     public GameObject pl1;
+    public GameObject tile_collision;
     public Rigidbody2D myRigidBody;
     public Sprite monochrome_tilemap_packed_240;
     private float x = 0;
     private float y = 0;
     public float jumpHeight = 0;
- 
+    private bool isOnGround = true;
     public float moveSpeed = 100;
 
     public void CreatePlayer(float Spawnx, float Spawny)
     {
-       
+        tile_collision = GameObject.FindGameObjectWithTag("tilemap_collision");
+        Floor tiles = tile_collision.GetComponent<Floor>();
         x=Spawnx; y=Spawny;
         gameObject.GetComponent<SpriteRenderer>().sprite = monochrome_tilemap_packed_240;
         pl1 = Instantiate(gameObject, new Vector3(x, y, 0), Quaternion.identity);
@@ -28,12 +30,22 @@ public class Player: MonoBehaviour
         //get the Input from Horizontal axis
         float horizontalInput = Input.GetAxis("Horizontal");
         bool isJumpInput = Input.GetKeyDown("space");
-        if (isJumpInput)
+        if (isJumpInput && isOnGround)
+        {
             myRigidBody.velocity = new Vector3(0, jumpHeight, 0);
+            isOnGround = false;
+        }
         //update the position
         pl1.transform.position += new Vector3(horizontalInput * moveSpeed * Time.deltaTime,0, 0);
 
         //output to log the position change
         Debug.Log(transform.position);
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == tiles.GetComponent<TilemapCollider2D>())
+        {
+            isOnGround= true;
+        }
     }
 }
